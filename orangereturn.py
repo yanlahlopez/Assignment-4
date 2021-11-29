8c2e901d27ac1648a3175239ff7873bb2dc9113d
Y-anlah Sophia Lopez <92627335+yanlahlopez@users.noreply.github.com>
2021-11-30 04:58:28 +0800

    Update orangereturn.py

--- orangereturn.py
+++ orangereturn.py
@@ -4,15 +4,16 @@ def quantity():
     apple = int(input("How many apples do you want? "))
     orange = int(input("How many oranges do you want? "))
     return apple, orange
-apple, orange=quantity()
 
 def product():
    appletotal = apple * 20
    orangetotal = orange * 25 
    return appletotal, orangetotal
-appletotal, orangetotal=product()
 
 def total(aamount,oamount):
     return aamount + oamount
+
+apple, orange=quantity()
+appletotal, orangetotal=product()
 
 print(f"The total amount is {total(appletotal,orangetotal)}.")
