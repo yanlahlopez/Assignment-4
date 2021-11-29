433a4e0a2a8b213265535519fa3b430888710c43
Y-anlah Sophia Lopez <92627335+yanlahlopez@users.noreply.github.com>
2021-11-30 04:59:24 +0800

    Update applereturn.py

--- applereturn.py
+++ applereturn.py
@@ -3,12 +3,13 @@ def data():
     money = float(input("Amount of money you have: "))
     appleprice = float(input("Enter price of apples: "))
     return money, appleprice
-money,appleprice=data()
 
 def amount(cash,price):
     return cash // price
     
 def applechange(cash, price):
     return cash % price
+
+money,appleprice=data()
 
 print(f"You can buy {amount(money,appleprice)} apple and your change is {applechange(money, appleprice):.2f} pesos.")
